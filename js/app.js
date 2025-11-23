6b6586fcb92200590391cec8f60296d047d35793
Khlad Thabit <khladxp@hotmail.com>
2025-11-23 21:41:35 +0300

    implemented scrollsmoother and better implementaion for scrolltrigger

--- js/app.js
+++ js/app.js
@@ -13,99 +13,61 @@ loadSVG()
 
 
 
-function setAnimationScroll(){
-    gsap.registerPlugin(ScrollTrigger);
 
+function setAnimationScroll() {
+    // 1. Register both plugins
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+    // 2. Initialize ScrollSmoother
+    ScrollSmoother.create({
+        wrapper: "#smooth-wrapper",
+        content: "#smooth-content",
+        smooth: 2.0,   // Duration of the smooth scroll (seconds)
+        effects: true, // Allows usage of data-speed attributes
+    });
+
+    // 3. Create the Timeline
     let runAnimation = gsap.timeline({
-        scrollTrigger:{
+        scrollTrigger: {
             trigger: "#story-parallax",
-            start: "top",
-            end: "bottom",
-            markers:true,
-            scrub: true,
-            pin:true,
+            start: "top top", // Start when top of element hits top of viewport
+            end: "+=10000", 
+            // scrub: true is instant (can be choppy). 
+            // scrub: 1 adds a 1-second "catch up" which masks jitter.
+            scrub: 1, 
+            pin: true,
+            // anticipatePin helps avoid a flash/jump when pinning starts
+            anticipatePin: 1 
         }
     });
-    
-    runAnimation.add([   
-        gsap.to('#Scene_1_fg_0', 0, {
-            y: -2500
-        }),           
-        
-        gsap.to('#Scene_1_layer_0', 0, {
-            y: -2200
-        }),            
-        
-        gsap.to('#Scene_1_fog_0', 0, {
-            y: -2200
-        }),           
-        gsap.to('#Scene_1_layer_1', 0, {
-            y: -2200
-        }),          
-        gsap.to('#Scene_1_fog_1', 0, {
-            y: -2200
-        }),         
-        
-        gsap.to('#Scene_1_Layer_2', 0, {
-            y: -2200
-        }),              
-        gsap.to('#Scene_1_Tower', 0, {
-            y: -2200
-        }),           
-        gsap.to('#Scene_1_bg', 0, {
-            y: -2200
-        }),      
 
-        gsap.to('#Scene_1_fg_1', 0, {
-            opacity : 0
-        })
-    ])
+    // 4. Add Animations (Using .from for better performance)
+    // The '0' at the end ensures all these start at time 0 simultaneously
+    runAnimation
+        .from('#Scene_1_fg_1', { opacity: 0, duration: 7 }, 5)
 
+        .from('#Scene_1_fg_0', { y: -2500, duration: 7 }, 0)
+        .from('#Scene_1_fg_1', { y: -2500, duration: 7 }, 0)
+        .from('#Scene_1_layer_0', { y: -2200, duration: 6 }, 0)
+        .from('#Scene_1_fog_0', { y: -2200, duration: 5 }, 0)
+        .from('#Scene_1_layer_1', { y: -2200, duration: 4 }, 0)
+        .from('#Scene_1_fog_1', { y: -2200, duration: 3 }, 0)
+        .from('#Scene_1_Layer_2', { y: -2200, duration: 3 }, 0)
+        .from('#Scene_1_Tower', { y: -2200, duration: 3 }, 0)
+        .from('#Scene_1_bg', { y: -2200, duration: 2.5 }, 0)
 
+        .to('.site-header', { opacity: 0, duration: 2.5 }, 0)
 
+        .to('#Scene_1_fg_0', { opacity: 0, duration: 2 }, 8) 
+        .to('#Scene_1_fg_1', { opacity: 1, duration: 1 }, 6)
 
-    .add([
-        gsap.to('#Scene_1_fg_0', 7, {
-            y: 0
-        }),   
-        gsap.to('#Scene_1_layer_0', 6, {
-            y: 0
-        }),   
-        gsap.to('#Scene_1_fog_0', 5, {
-            y: 0
-        }),         
-        gsap.to('#Scene_1_layer_1', 4, {
-            y: 0
-        }), 
-        gsap.to('#Scene_1_fog_1', 3, {
-            y: 0
-        }),         
-        
-        gsap.to('#Scene_1_Layer_2', 3, {
-            y: 0
-        }),             
-        gsap.to('#Scene_1_Tower', 3, {
-            y: 0
-        }),          
-        gsap.to('#Scene_1_bg', 2.5, {
-            y: 0
-        }), 
-        gsap.to('.site-header', 2.5, {
-            opacity: 0
-        }),  
-    ])
-
-
-
-
-
-    .add([
-        gsap.to('#Scene_1_fg_0', 2, {
-            opacity: 0
-        }),        
-        gsap.to('#Scene_1_fg_1', 1, {
-            opacity: 1
-        })
-    ])
+        .to('#Scene_1_fg_0', { y: -2500, duration: 7 }, 10)
+        .to('#Scene_1_fg_1', { y: -2500, duration: 7 }, 10)
+        .to('#Scene_1_layer_0', { y: -2200, duration: 6 }, 10)
+        .to('#Scene_1_fog_0', { y: -2200, duration: 5 }, 10)
+        .to('#Scene_1_layer_1', { y: -2200, duration: 4 }, 10)
+        .to('#Scene_1_fog_1', { y: -2200, duration: 4 }, 10)
+        .to('#Scene_1_Layer_2', { y: -2200, duration: 4 }, 10)
+        .to('#Scene_1_Tower', { y: -2200, duration: 4 }, 10)
+        .to('#Scene_1_bg', { y: -2200, duration: 3 }, 10);
 }
-
